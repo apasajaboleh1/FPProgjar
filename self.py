668d922270eb7caf5b181d799dd10ae3f661dec6
apasajaboleh1 <freddy.yuwono@gmail.com>
2015-12-18 22:31:41 +0700

    loadbalancer second tier

--- self.py
+++ self.py
@@ -3,26 +3,8 @@ import SocketServer
 import socket
 list_server = []
 
-'''
-for i in range(2):
-	list_server.append('10.151.34.120')
-	list_server.append('9009')
-
-for i in range(2):
-	list_server.append('10.151.34.104')
-	list_server.append('9009')
-	
-for i in range(6):
-	list_server.append('10.151.34.147')
-	list_server.append('9009')
-
-for i in range(4):
-	list_server.append('10.151.34.105')
-	list_server.append('9009')
-'''
 for i in range(5):
 	list_server.append(socket.gethostbyname(socket.gethostname()))
-	#list_server.append('localhost')
 	list_server.append('%d'%(9010+i))
 
 idx = 0
@@ -32,14 +14,13 @@ class myHandler(SimpleHTTPServer.SimpleHTTPRequestHandler):
 		#print self.path
 		global idx
 		self.send_response(303)
-		print 'fuk yu', idx
 		new_path = 'http://%s:%s' % (list_server[idx],list_server[idx+1])
 		idx = idx+2
 		
 		if idx >= len(list_server):
 			idx=0
 		
-		#print 'fuk yu',new_path
+		
 		self.send_header('Location', new_path)
 		self.end_headers()
 
