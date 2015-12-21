b87bedfd0e194da611dff417b7380a0989507ab8
leonspirit <archmage_louis@yahoo.co.id>
2015-12-21 11:39:19 +0700

    Script also run load balancer

--- run-server.bat
+++ run-server.bat
@@ -3,3 +3,4 @@ start cmd.exe /C  win-run/spawn-server2.bat
 start cmd.exe /C  win-run/spawn-server3.bat
 start cmd.exe /C  win-run/spawn-server4.bat
 start cmd.exe /C  win-run/spawn-server5.bat
+start cmd.exe /C  python self.py
